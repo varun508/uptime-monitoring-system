61c1b8112b3d3200359b2f7033f4b26484bdfaa4
Varun Sharma <imva508@gmail.com>
2019-03-20 14:01:08 +0530

    created custom routers and handlers for the incoming requests

--- index.js
+++ index.js
@@ -22,25 +22,65 @@ const server = http.createServer((req, res) => {
     // Extract all headers
     let headers = req.headers;
 
+    // Extract the method
+    let method = req.method.toLowerCase();
+
     // Get the payload, if any
     let decoder = new StringDecoder('utf-8');
     let buffer = '';
 
-    req.on('data', data => {
-        buffer += decoder.write(data);
-    });
+    req.on('data', data => buffer += decoder.write(data));
 
     req.on('end', () => {
-        buffer += decoder.end();
-        console.log('here\'s the buffer: ' + buffer);
+        buffer += decoder.end()
+
+        // Choose the handler for incoming request. If the handler is not found then use handlers.notFound
+        let handler = router[trimmedPath]
+        let chosenHandler = typeof (handler) !== 'undefined' ? handler : handlers.notFound;
+
+        // Bundle all the extracted data parts in an Object
+        let data = {
+            'path': trimmedPath,
+            'payload': buffer,
+            'headers': headers,
+            'queryObjects': queryStringObject,
+            'method': method
+        }
+
+        chosenHandler(data, (statusCode, responsePayload) => {
+
+            // Set the statusCode to 200 HTTP_OK when the statusCode is of type other than the number
+            statusCode = typeof (statusCode) !== 'number' ? statusCode : 200;
+
+            // Return an empty object if the payload is not an object
+            responsePayload = typeof(responsePayload) !== 'object' ? responsePayload : {};
+            
+            res.writeHead(statusCode);
+            res.end(responsePayload);
+        })
+
+        res.end(buffer);
     });
-
-    // Send the response
-    console.log(headers)
-
-    res.end();
 })
 
-server.listen(3000, () => {
-    console.log(`Listening on port 3000..`)
-})
+
+// Make the server listen to requests on port 3000
+server.listen(3000, () => console.log(`Listening on port 3000..`))
+
+// Define request handlers
+const handlers = {
+    // Handler for sample route
+    sample: (data, callback) => {
+        callback(406, '{ name: sampleHadler }');
+    },
+
+    // Handler for all request coming to unknown paths
+    notFound: (data, callback) => {
+        callback(404)
+    }
+}
+
+// Define the request routers
+const router = {
+    'sample': handlers.sample
+}
