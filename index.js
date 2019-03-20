da23134f90152d3ffb50130ed6e14e51a95996b3
Varun Sharma <imva508@gmail.com>
2019-03-20 12:25:28 +0530

    payload extraction

--- index.js
+++ index.js
@@ -2,21 +2,43 @@
  * Primary file for the API
  */
 
-import { createServer } from 'http';
-import { parse } from 'url';
+const http = require('http');
+const url = require('url');
+const StringDecoder = require('string_decoder').StringDecoder;
 
 
-const server = createServer((req, res) => {
+const server = http.createServer((req, res) => {
 
     // Get the url and parse it
-    let parsedUrl = parse(req.url, true);
+    let parsedUrl = url.parse(req.url, true);
 
     // Get the path
     let path = parsedUrl.pathname;
     let trimmedPath = path.replace(/^\/+|\/+$/g, '')
 
+    // Extract the query parameters 
+    let queryStringObject = parsedUrl.query;
+
+    // Extract all headers
+    let headers = req.headers;
+
+    // Get the payload, if any
+    let decoder = new StringDecoder('utf-8');
+    let buffer = '';
+
+    req.on('data', data => {
+        buffer += decoder.write(data);
+    });
+
+    req.on('end', () => {
+        buffer += decoder.end();
+        console.log('here\'s the buffer: ' + buffer);
+    });
+
     // Send the response
-    res.end('path is ' + trimmedPath);
+    console.log(headers)
+
+    res.end();
 })
 
 server.listen(3000, () => {
