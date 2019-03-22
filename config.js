4df1aa4cedb2547f016a09b40fe53929952d0e83
Varun Sharma <imva508@gmail.com>
2019-03-22 13:21:27 +0530

    added the helper for hashing the password

--- config.js
+++ config.js
@@ -6,25 +6,27 @@ const environment = {};
 // Development environment {DEFAULT}
 environment.development = {
     'port': 3000,
-    'envName': 'development'
+    'envName': 'development',
+    'hashingSecret': 'This is a secret'
 }
 
 // Production environments
 environment.production = {
     'port': 4200,
-    'envName': 'production'
+    'envName': 'production',
+    'hashingSecret': 'This is a secret'
 }
 
-let env = process.env.NODE_ENV 
+let env = process.env.NODE_ENV
 
 // Get the environment passed as the command-line argument
-let passedEnvironment = typeof(env) == 'string' ? env : '';
+let passedEnvironment = typeof (env) == 'string' ? env : '';
 
 // Get the environment object for the environment passed
 let currentEnvironment = environment[passedEnvironment];
 
 // Set the environment to default if the currentEnvironment is not valid
-let finalEnvironment = typeof(currentEnvironment) == 'object' ? currentEnvironment : environment.development;
+let finalEnvironment = typeof (currentEnvironment) == 'object' ? currentEnvironment : environment.development;
 
 // Export the environment
 module.exports = finalEnvironment; 
